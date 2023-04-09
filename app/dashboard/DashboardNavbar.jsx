28f7e903739dd606148f1b4612d283d225c3f8d2
mitul <mitul@DESKTOP-9F3VBN9.lan>
2023-04-10 00:05:14 +0200

    dashboard error solve by hasan

--- app/dashboard/DashboardNavbar.jsx
+++ app/dashboard/DashboardNavbar.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import MenuItems from "../components/dashboard/Menuitem";
+import React from 'react';
+import MenuItems from '../components/MenuItems';
+// import MenuItems from "../components/dashboard/Menuitem";
 
 const DashboardNavbar = () => {
   return (
-    <div className="p-2 flex flex-col justify-items-center">
-      <div className="flex-1">
-        <div className="mb-4">
+    <div className='p-2 flex flex-col justify-items-center'>
+      <div className='flex-1'>
+        <div className='mb-4'>
           <img
-            className="rounded-full w-5/6 mx-auto"
-            src="https://static.vecteezy.com/system/resources/previews/002/002/403/original/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
-            alt=""
+            className='rounded-full w-5/6 mx-auto'
+            src='https://static.vecteezy.com/system/resources/previews/002/002/403/original/man-with-beard-avatar-character-isolated-icon-free-vector.jpg'
+            alt=''
           />
         </div>
         <div>
-          <MenuItems title="Profile" address="/dashboard/profile"></MenuItems>
-          <MenuItems title="Profile" address="/dashboard/profile"></MenuItems>
-          <MenuItems title="Profile" address="/dashboard/profile"></MenuItems>
-          <MenuItems title="Profile" address="/dashboard/profile"></MenuItems>
-          <MenuItems title="Profile" address="/dashboard/profile"></MenuItems>
+          <MenuItems title='Profile' address='/dashboard/profile'></MenuItems>
+          <MenuItems title='Profile' address='/dashboard/profile'></MenuItems>
+          <MenuItems title='Profile' address='/dashboard/profile'></MenuItems>
+          <MenuItems title='Profile' address='/dashboard/profile'></MenuItems>
+          <MenuItems title='Profile' address='/dashboard/profile'></MenuItems>
         </div>
       </div>
-      <div className="h-20">
-      </div>
+      <div className='h-20'></div>
     </div>
   );
 };
