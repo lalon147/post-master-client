7f975e9f10795feb6281db03ab52fc0808ebab8b
mitulchy07 <100080242+mitulchy07@users.noreply.github.com>
2023-04-09 22:04:32 +0200

    test

--- app/components/home/Footer.jsx
+++ app/components/home/Footer.jsx
@@ -1,27 +1,27 @@
-import Image from "next/image";
-import Layout from "../Layout";
+import Image from 'next/image';
+import Layout from '../Layout';
 
 const Footer = () => {
   return (
-    <div className="bg-base-200">
+    <div className='bg-base-200'>
       <Layout>
         <footer
-          className="p-10 py-20 text-base-content rounded"
+          className='p-10 py-20 text-base-content rounded'
           style={{ backgroundImage: `url('/public/images/bg-footer.png')` }}
         >
-          <div className="flex justify-between ">
+          <div className='flex justify-between '>
             <div>
-              <h1 className="text-gray-400 text-lg font-bold">SERVICES</h1>
-              <p className="text-gray-600 text-sm font-normal">lorem ipsum</p>
+              <h1 className='text-gray-400 text-lg font-bold'>SERVICES</h1>
+              <p className='text-gray-600 text-sm font-normal'>lorem ipsum</p>
             </div>
             <div>
-              <h1 className="text-gray-400 text-lg font-bold">OUR ADDRESS</h1>
-              <p className="text-gray-600 text-sm font-normal">
-                New York - 101010 Hudson
+              <h1 className='text-gray-400 text-lg font-bold'>OUR ADDRESS</h1>
+              <p className='text-gray-600 text-sm font-normal'>
+                Cologne, Germany.
               </p>
             </div>
           </div>
-          <p className="text-sm text-center mt-20">
+          <p className='text-sm text-center mt-20'>
             Copyright 2022 All Rights Reserved
           </p>
         </footer>
